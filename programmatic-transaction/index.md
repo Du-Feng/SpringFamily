fde698b9c809947e1beb4f4b6b81d1253ab859b7
龚靖 <huhuhetutu@yeah.net>
2020-10-01 10:06:19 +0800

    Add the TransactionTemplate example code.

---
+++ programmatic-transaction/index.md
@@ -0,0 +1,4 @@
+# Programmatic Transaction
+
+1. 在 https://start.spring.io 的配置如下：
+![Spring Initializer](assets/images/spring.initializr.png)
--- programmatic-transaction/src/main/java/com/example/programmatictransaction/ProgrammaticTransactionApplication.java
+++ programmatic-transaction/src/main/java/com/example/programmatictransaction/ProgrammaticTransactionApplication.java
@@ -1,13 +1,43 @@
 package com.example.programmatictransaction;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @SpringBootApplication
-public class ProgrammaticTransactionApplication {
+@Slf4j
+public class ProgrammaticTransactionApplication implements CommandLineRunner {
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProgrammaticTransactionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammaticTransactionApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("COUNT BEFORE TRANSACTION: {}", getCount());
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus transactionStatus) {
+                jdbcTemplate.execute("INSERT INTO FOO (ID, BAR) VALUES (1, 'aaa')");
+                log.info("COUNT IN TRANSACTION: {}", getCount());
+                transactionStatus.setRollbackOnly();
+            }
+        });
+        log.info("COUNT AFTER TRANSACTION: {}", getCount());
+    }
+
+    private long getCount() {
+        //return (long) jdbcTemplate.queryForList("SELECT COUNT(*) AS CNT FROM FOO").get(0).get("CNT");
+        return jdbcTemplate.queryForObject("SELECT COUNT(*) AS CNT FROM FOO", long.class);
+    }
 }
