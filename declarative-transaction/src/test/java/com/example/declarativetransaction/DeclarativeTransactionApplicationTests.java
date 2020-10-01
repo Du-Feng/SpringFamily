73f25b8d60496617ffc524d5cab92d6802b76685
龚靖 <huhuhetutu@yeah.net>
2020-10-01 10:19:30 +0800

    Initialize the declarative transaction project.

---
+++ declarative-transaction/src/test/java/com/example/declarativetransaction/DeclarativeTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.declarativetransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclarativeTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
