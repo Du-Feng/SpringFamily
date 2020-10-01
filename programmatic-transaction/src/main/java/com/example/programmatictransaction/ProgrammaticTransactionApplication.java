265d64b3891cdcb8bf00239c862c1f7f07efd120
龚靖 <huhuhetutu@yeah.net>
2020-10-01 09:46:47 +0800

    Initialize the programmatic transaction project.

---
+++ programmatic-transaction/src/main/java/com/example/programmatictransaction/ProgrammaticTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.programmatictransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammaticTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammaticTransactionApplication.class, args);
+	}
+
+}
---
+++ programmatic-transaction/src/test/java/com/example/programmatictransaction/ProgrammaticTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.programmatictransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammaticTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
