5fcdcd497c6e256b7b3e9b0cc0ad172d1bd5ce44
龚靖 <huhuhetutu@yeah.net>
2020-10-02 08:05:20 +0800

    Initialize the error code project.

---
+++ errorcode/src/test/java/com/example/errorcode/ErrorcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.errorcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
