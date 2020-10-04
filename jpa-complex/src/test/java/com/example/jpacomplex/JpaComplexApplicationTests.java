884969235f5a8009767e5bc5f3d0a588747bf09e
龚靖 <huhuhetutu@yeah.net>
2020-10-04 08:15:45 +0800

    Initialize the JPA complex project.

---
+++ jpa-complex/src/test/java/com/example/jpacomplex/JpaComplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpacomplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaComplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
