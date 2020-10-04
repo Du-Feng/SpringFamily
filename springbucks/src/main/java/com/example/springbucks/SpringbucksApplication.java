7a7497ddd7f32f0ba91e015ebc390ab569ce057f
Feng Du <feng.du@outlook.com>
2020-10-04 16:59:18 +0800

    Initialize the springbucks project.

---
+++ springbucks/src/main/java/com/example/springbucks/SpringbucksApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbucksApplication.class, args);
+	}
+
+}
---
+++ springbucks/src/test/java/com/example/springbucks/SpringbucksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbucksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
