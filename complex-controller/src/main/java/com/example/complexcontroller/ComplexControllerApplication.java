29cdae297e8f014dc70873cbfaa4f224b9cc804f
Feng Du <feng.du@outlook.com>
2020-10-13 11:43:30 +0800

    Add the complex controller project.

---
+++ complex-controller/src/main/java/com/example/complexcontroller/ComplexControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.complexcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexControllerApplication.class, args);
+	}
+
+}
